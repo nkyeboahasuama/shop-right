2756fa74b3e0c796ee17879f11e9a276ddce107d
Nana <ynkwame@gmail.com>
2024-02-23 00:26:54 -0600

    Added user and order services

--- src/core/interface.ts
+++ src/core/interface.ts
@@ -6,3 +6,18 @@ export interface IProduct {
   image?: string;
   sizes: string[];
 }
+
+export interface IOrder {
+  _id?: string;
+  customerName: string;
+  products: string[];
+  totalAmount: number;
+  time?: string;
+}
+
+export interface IUser {
+  _id?: string;
+  name: string;
+  email: string;
+  time?: string;
+}
--- src/service/users/user.service.ts
+++ src/service/users/user.service.ts
@@ -1,6 +1,16 @@
-const createUser = () => {
-  // post data from api
-  // return data
+import axios from "axios";
+import { IUser } from "../../core/interface";
+
+const USER_API = "http://localhost:3001/api/user";
+
+const createUser = async (newUser: IUser) => {
+  try {
+    const response = await axios.patch(`${USER_API}`, newUser);
+    console.log(response);
+    return response;
+  } catch (error) {
+    console.log(error);
+  }
 };
 
 export const userService = { createUser };
